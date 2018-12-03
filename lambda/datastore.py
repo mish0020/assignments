889aa78f2f56adbf2690be411bf60dfc7283af48
EC2 Default User <ec2-user@ip-172-31-83-42.ec2.internal>
2018-12-03 04:53:39 +0000

    batch

--- lambda/datastore.py
+++ lambda/datastore.py
@@ -17,16 +17,6 @@ def db_writer(event, context):
     decoded_record_data = [base64.b64decode(record['kinesis']['data']) for record in event['Records']]
     deserialized_data = [json.loads(decoded_record) for decoded_record in decoded_record_data]
 
-    #with table.batch_writer() as batch_writer:
-    for item in deserialized_data:
-       id = item['id']
-       x = item['x']
-       y = item['y']
-       is_hot = item['is_hot']
-       table.put_item(Item={
-            'id': id,
-            'x':x,
-            'y':y,
-            'is_hot':is_hot,
-          }) 
-       print(json.dumps(item))
+    with table.batch_writer() as batch_writer:
+       for item in deserialized_data:
+          batch_writer.put_item(Item=item)
